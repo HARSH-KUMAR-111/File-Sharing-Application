8e99c4846bdfd6f9c111be888726fcd54b4e2278
Harsh kumar <hk342064@gmail.com>
2024-07-09 03:21:29 +0530

    Alert MSG

--- app/(dashboard)/upload/_components/UploadForm.js
+++ app/(dashboard)/upload/_components/UploadForm.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
+import AlertMsg from './AlertMsg';
 
 function uploadForm() {
   const [file,setFile] = useState();
   const onFileSelect=(file)=>{
     console.log(file)
+
+    if(file&& file.size>2000000) {
+        console.log("size is greater than 2 MB.")
+        return ;
+    }
+    setFile(file)
   }
   return (
     <div className='text-center'>
@@ -12,7 +19,7 @@ function uploadForm() {
 class="flex items-center justify-center w-full">
     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-red-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg class="w-12 h-12 mb-5 text-red-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+            <svg class="w-12 h-12 mb-5 text-red-600 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
             </svg>
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-bold">Click to upload</span> or <span class='text-red-500 font-semibold'>drag</span> and <span class='text-red-500 font-semibold'>drop</span></p>
@@ -21,6 +28,7 @@ class="flex items-center justify-center w-full">
         <input id="dropzone-file" type="file" class="hidden" onChange={(event)=>onFileSelect(event.target.files[0])}/>
     </label>
 </div> 
+    <AlertMsg msg={'Maximum file is 2 MB'}/>
     <button disabled={!file} className='p-2 bg-red-500 text-white w-[30%] rounded-full mt-5 disabled:bg-gray-400'>Upload</button>
 
     </div>
