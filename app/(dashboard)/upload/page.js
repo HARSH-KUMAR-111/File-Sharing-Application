368c27687347e50fae71189ec96b48d79e6ba65b
Harsh kumar <hk342064@gmail.com>
2024-07-09 02:22:50 +0530

    upload form page

--- app/(dashboard)/upload/page.js
+++ app/(dashboard)/upload/page.js
@@ -1,9 +1,15 @@
+"use client"
 import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+import UploadForm from './_components/UploadForm';
 
 function Upload() {
   return (
-    <div>
+    <div className='p-5 px-8 md:px-28'>
       Upload
+      <h2 className='text-[20px] text-center m-5'> Start 
+        <span className="text-red-600 font-bold"> Uploading</span> and <span className="text-red-600 font-bold">Sharing</span> File </h2>
+      <UploadForm/>
       <UserButton afterSignOutUrl='/'/>
     </div>
   )
